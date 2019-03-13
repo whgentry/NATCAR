c86103bd985d762437c8a286b4c55091081d7f07
jhoang209 <41424708+jhoang209@users.noreply.github.com>
2019-03-13 14:14:15 -0700

    Add files via upload

--- main3.py
+++ main3.py
@@ -1,4 +1,3 @@
-# LAB3
 # Jimmy Hoang
 # William Gentry
 # Daniel Burr
@@ -10,7 +9,8 @@ from motors import DCMotor, ServoMotor
 from bluetooth import Bluetooth
 
 ##### Global Variables and Initialization #####
-## uart
+
+## UART
 uart = UART(1, 115200)
 bt = Bluetooth(uart)
 
@@ -21,52 +21,48 @@ dc_motor.set_current_sense(cs="P6", cs_dis="P9")
 dutycyclePW = 0
 
 ## Servo motor control
-servo_max = 0.0019
-servo_min = 0.0010
+servo_max = 0.0019 #turning left
+servo_min = 0.001 #turning right
 servo_center = (servo_max + servo_min) / 2
 servo_offset = servo_max - servo_center
 servo_motor = ServoMotor(tim_num=4, channel=1, frequency=300, pin="P7")
 servo_motor.set_range(max_pw=servo_max, min_pw=servo_min)
 sec = servo_center
+servo_motor.set_pulse_width(sec)
 
 enable = 0
 
 ## Camera Control
 thresholds = (265, 275) #245 to 255
 
-max_roi = 50
-min_roi = 40
-diff_roi = max_roi - min_roi
-percent_change = 0
-
-roi1 = (0, 0, 160, 10)
-
 roi_s1 = 10
 roi_s2 = 40
 roi_num = 6
 roi_dist = 5
-roi_array1 = [(0,roi_s1,160,10)] * roi_num
-roi_array2 = [(0,roi_s2,160,10)] * roi_num
+
+roi_array_1 = [(0,roi_s1,160,10)] * roi_num
+roi_array_2 = [(0,roi_s2,160,10)] * roi_num
 
 for i in range(roi_num):
-    roi_array1[i] = (0,roi_s1 + roi_dist * i,160,10)
-    roi_array2[i] = (0,roi_s2 + roi_dist * i,160,10)
+    roi_array_1[i] = (0,roi_s1 + roi_dist * i,160,10)
+    roi_array_2[i] = (0,roi_s2 + roi_dist * i,160,10)
 
 dist_array = [0] * roi_num
 center = 80
 
-
 dist = lambda cb,cr: abs(cr-cb)
 pin1 = Pin('P1', Pin.OUT_PP, Pin.PULL_NONE)
+
 x_1 = 80
+x_2 = 80
 y_1 = 0
-x_2 = 0
 y_2 = 1
+
 x_diff1 = 0
 x_err1 = [0, 0, 0, 0]
 dist2center = 0
-blobs1 = []
 motor = 0
+
 # Configure camera
 sensor.reset()                      # Reset and initialize the sensor.
 sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
@@ -77,26 +73,17 @@ sensor.set_auto_whitebal(False) # must be turned off for color tracking
 clock = time.clock()                # Create a clock object to track the FPS.
 
 ## Control Values
-# PID#2.5
-Kp_max_s = 1.4
+# PID
+Kp_s = 1.4
 Kd_s = 0.014
 max_pwm = 35
 min_pwm = 25
+
 # brake control
-straight_counter = 0
-brake_wait_counter = 0
-brake_wait = 50
+turning = 0
+braking = 0
 brake_counter = 0
-brake = 10
-turn_thres = 0.5
-straight = 0
-straight_counter = 0
-brake_counter = 0
-S = 40
-# manual control
-command_str = "0"
-dc_command = 0
-ser_command = 0.00145 # value for straight
+brake_max = 20
 
 ##### MAIN LOOP #####
 while(True):
@@ -108,56 +95,33 @@ while(True):
         if (cmd == "x"):
             enable = 0
 
-        if (cmd == "Kp_max"):
-            Kp_max_s = value
-        if (cmd == "KD"):
+        if (cmd == "Kp"):
+            Kp_s = value
+        if (cmd == "Kd"):
             Kd_s = value
-        if (cmd == "S"):
-            S = value
+        if (cmd == "b"):
+            brake_max = value
+
         if (cmd =="max_pwm"):
             max_pwm = value
         if (cmd == "min_pwm"):
             min_pwm = value
 
-        if (cmd == "servo_max"):
-            servo_max = value
-            sec = servo_max
-        if (cmd == "servo_min"):
-            servo_min = value
-            sec = servo_min
-
-        if (cmd == "max_roi"):
-            max_roi = value
-            diff_roi = max_roi - min_roi
-        if (cmd == "min_roi"):
-            min_roi = value
-            diff_roi = max_roi - min_roi
-
-        if (cmd == "brake_wait"):
-            brake_wait = value
-        if (cmd == "brake"):
-            brake = value
-        if (cmd == "turn_thres"):
-            turn_thres = value
-
-
     clock.tick()                    # Update the FPS clock.
     img = sensor.snapshot()         # Take a picture and return the image.
     #print(clock.fps())
 
-
     ##### Blob Detection #####
-    instant_change = abs((servo_center - sec)/servo_offset)
 
-    # Brake Roi
+    #ROI_ARRAY1
     for i in range(roi_num):
-        blobs = img.find_blobs([thresholds], roi = roi_array1[i], pixels_threshold=10, area_threshold=10)
+        blobs = img.find_blobs([thresholds], roi = roi_array_1[i], pixels_threshold=10, area_threshold=10)
 
         if (len(blobs) > 0):
             if (len(blobs) == 3 and i == 0):
                 line1_dist = abs(blobs[1].cx() - blobs[0].cx())
                 line2_dist = abs(blobs[1].cx() - blobs[2].cx())
-                if (line1_dist < 25 and line1_dist > 15 and line2_dist < 25 and line2_dist > 15):
+                if (line1_dist < 25 and line1_dist > 10 and line2_dist < 25 and line2_dist > 10):
                     print(line1_dist)
                     print("3 lines detected")
                     enable = 0
@@ -174,10 +138,12 @@ while(True):
                 dist_array[i] = minblob.cx()
 
     x_1 = int(sum(dist_array) / len(dist_array))
+    y_1 = 10
     img.draw_cross(x_1, 120, color = 0)
 
+    #ROI_ARRAY2
     for i in range(roi_num):
-        blobs = img.find_blobs([thresholds], roi = roi_array2[i], pixels_threshold=10, area_threshold=10)
+        blobs = img.find_blobs([thresholds], roi = roi_array_2[i], pixels_threshold=10, area_threshold=10)
 
         if (len(blobs) > 0):
             minblob = blobs[0]
@@ -191,7 +157,11 @@ while(True):
             dist_array[i] = minblob.cx()
 
     x_2 = int(sum(dist_array) / len(dist_array))
+    y_2 = 40
     img.draw_cross(x_2, 120, color = 0)
+
+    angle = math.atan((x_1 - x_2) / (y_2 - y_1))
+    angle = math.degrees(angle)
 
     ##### SERVO MOTOR CONTROL #####
 
@@ -203,8 +173,7 @@ while(True):
 
     #PROPORTIONAL
     dist2center = center - x_1
-    sec = servo_center + servo_offset*( ((dist2center/center) * (Kp_max_s)) + (x_diff1 * Kd_s) )
-    #sec = servo_center
+    sec = servo_center + servo_offset*( ((dist2center/center) * (Kp_s)) + (x_diff1 * Kd_s) )
 
     if sec > servo_max :
         sec = servo_max
@@ -213,30 +182,32 @@ while(True):
 
     #DC MOTOR CONTROL
     if (enable == 0):
-        servo_motor.set_range(max_pw=servo_max, min_pw=servo_min)
         dc_motor.brake_vcc()
 
     elif (enable == 1):
-        if(brake_counter == 0):
-            dc_motor.forward()
-        else:
-            dc_motor.brake_vcc()
-            brake_counter -= 1
 
-
-        straight = (abs(dist2center) < S )
-        if straight:
-            straight_counter += 1
-
-        elif straight_counter > 70:
-            straight_counter = 0
-            brake_counter = 10
-
-        else:
-            straight_counter = 0
-
+        dc_motor.forward()
         dutycyclePW =  max_pwm  - (abs(dist2center/center) * (max_pwm - min_pwm)) #DC
+        print(angle)
+        print(turning)
+        print(braking)
 
+        if (turning == 1):
+            if (braking == 1):
+                if(brake_counter <= brake_max):
+                    dc_motor.brake_gnd()
+                    brake_counter += 1
+                else:
+                    dc_motor.forward()
+                    braking = 0
+
+            if (abs(angle) < 5 and abs(angle) != 0):
+                turning = 0
+        else:
+            if (abs(angle) > 10):
+                turning = 1
+                braking = 1
+                brake_counter = 0
 
     dc_motor.set_duty_cycle(dutycyclePW)
     servo_motor.set_pulse_width(sec)
